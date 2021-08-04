8fb5dd3c71c22effef852a31955237bd1eed8076
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-08-04 16:55:30 -0700

    Delete tokens.py

--- tokens.py
+++
@@ -1,6 +0,0 @@
-# Create an app on the spotify developers site and follow the instructions on [https://developer.spotify.com/documentation/general/guides/authorization-guide/] to get your tokens.
-
-spotify_user_id = ''
-refresh_token = ''
-access_token = ''
-
