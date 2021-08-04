47780823cdbb7333cb95ec61139630261643dfa8
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-08-04 16:52:19 -0700

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,3 +1,6 @@
-spotify_token = ''
+# Create an app on the spotify developers site and follow the instructions on [https://developer.spotify.com/documentation/general/guides/authorization-guide/] to get your tokens.
+
 spotify_user_id = ''
+refresh_token = ''
+access_token = ''
 
